a28dab9063e9c42d03f37e47780214eaf2342551
Li WeiHung <leeweihong451@gmail.com>
2024-07-17 23:52:07 +0800

    Revise demo bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { useMemo, useState } from 'react';
+import UploadComponent from "./component/UploadComponent.tsx"
 import { LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined, PayCircleOutlined } from '@ant-design/icons';
 import { Steps } from 'antd';
 import runStep from "./demo.ts"
+import "./index.sass"
 const App: React.FC = () => {
+  interface updateProps{
+    title:string,
+    newStatus:string,
+    isLoading:boolean
+  }
   //const steps = ["process","finish"];
   const iconStatus = {"Login":<UserOutlined />,"Verification":<SolutionOutlined />,"Pay":<PayCircleOutlined />,"Done":<SmileOutlined />,"Loading":<LoadingOutlined/>};
-  const [items,setItems] = useState([{"title": 'Login',"status": 'wait',"icon": <UserOutlined />},
+  const [items,setItems] = useState([{"title": 'Login',"status": 'process',"icon": <LoadingOutlined/>},
     {
       "title": 'Verification',
       "status": 'wait',
@@ -20,17 +27,36 @@ const App: React.FC = () => {
       "icon": <SmileOutlined />
     }]);
 
-    const updateItem = (title:string,newStatus:string,isLoading:boolean)=>{
-      setItems(items.map((item)=>{
-        return item.title===title && item.status!==newStatus?(isLoading===true?{...item,status: newStatus,icon:iconStatus["Loading"]}:{...item,status: newStatus,icon:iconStatus[title]}):item
-      }))
+    const updateItem = (type:number,index:number,title:string,newStatus:string,isLoading:boolean)=>{
+      if(type===1){
+        console.log("type1");
+        
+        setItems(items.map((item)=>{
+          return item.title===title && item.status!==newStatus?(isLoading===true?{...item,status: newStatus,icon:iconStatus["Loading"]}:{...item,status: newStatus,icon:iconStatus[title]}):item
+        }))
+      }
+      else{
+        //console.log("type2, "+items[index+1].title+"Real: "+item.title);
+        setItems(items.map((item)=>{
+          console.log("type2, "+items[index+1].title+"  Real: "+item.title);
+          return item.title===title && item.status!==newStatus?{...item,status: newStatus,icon:iconStatus[title]}: item.title===items[index+1].title? item.title==="Done" ?{...item,status: 'finish',icon:iconStatus[item.title]}:{...item,status: 'process',icon:iconStatus["Loading"]}:item
+        }))
+      }
     }
     
   runStep(items,updateItem);
 
 
     return(
-      <Steps items = {items}/>
+      <>
+        <h1 className="title">Upload_Tool</h1>
+        <div className = "uploadunit">
+        <UploadComponent/>
+        </div>
+        <div className = "stepunit">
+          <Steps items = {items}/>
+        </div>
+      </>
     )
 };
   
--- src/demo.ts
+++ src/demo.ts
@@ -2,37 +2,23 @@ import React, { useState } from 'react';
 
 const runStep = (items,updateItem)=>{
     const steps = ["process","finish"];
-    const [indexs,setIndex] = useState([0,0])
+    const [indexs,setIndex] = useState([0,1])
     let i = indexs[0];
     let j = indexs[1];
     if(i<3){
       setTimeout(()=>{
           console.log("i:"+i+"||j:"+j);
           if (steps[j]=="process"){
-              updateItem(items[i].title,steps[j],true);
-          }
-          else{
-              updateItem(items[i].title,steps[j],false);
-          }
-          if(j<1){
+              updateItem(1,i,items[i].title,steps[j],true);
               j += 1;
-          } 
+          }
           else{
+              updateItem(2,i,items[i].title,steps[j],false);
               i += 1;
-              if (i==3){
-                  j = 1;
-                  //updateItem(items[i].title,steps[j],false);
-              }else{
-                  j = 0;
-              }
+              j = 1;
           }
           setIndex([i,j]);
-      },1000);
-    }
-    else if (i==3){
-      updateItem(items[i].title,steps[j],false);
-      i+=1;
-      setIndex([i,j]);
+      },3000);
     }
 }
 
